6e5b3831db55281a22cc54b4b7f6f193cdb2bf1b
bay1226 <373631631@qq.com>
2016-06-30 17:50:42 +0800

    add

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-	'DB_TYPE' => 'mysqli',
+	'DB_TYPE' => 'mysql',
 	'DB_HOST' => 'localhost',
 	'DB_NAME' => 'app_manji',
 	'DB_USER' => 'root',
-	'DB_PWD' => 'bay123',
+	'DB_PWD' => 'kwr',
 	'DB_PORT' => 3306,
 );
